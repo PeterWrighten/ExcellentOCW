de3689da2c6f51dcfabdcd9cc738c5c656cf2fc2
PeterWrighten <peterwrighten@gmail.com>
2021-10-21 16:05:04 +0900

    update

---
+++ MIT_6.S096/assn8/operator_override.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Complex {
+private:
+	int real, imag;
+public:
+	Complex(int r = 0, int i =0) {real = r; imag = i;}
+	void print() { cout << real << " + i" << imag << endl; }
+
+// The global operator function is made friend of this class so
+// that it can access private members
+friend Complex operator + (Complex const &, Complex const &);
+};
+
+
+Complex operator + (Complex const &c1, Complex const &c2)
+{
+	return Complex(c1.real + c2.real, c1.imag + c2.imag);
+}
+
+
+int main()
+{
+	Complex c1(10, 5), c2(2, 4);
+	Complex c3 = c1 + c2; // An example call to "operator+"
+	c3.print();
+	return 0;
+}
