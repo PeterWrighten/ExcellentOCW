210583b0ec5789c865a07d7300e1ad97ae9bfe69
PeterWrighten <peterwrighten@gmail.com>
2021-11-04 03:18:22 +0900

    update demo java.util.Scanner

---
+++ MIT_6.170/assn1/demo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class demo{
+	public static void main(String[] args){
+		Scanner n = new Scanner(System.in);
+		String str = n.next();
+		int x = n.nextInt();
+		System.out.println(str);
+		System.out.println(x);
+	}
+}
